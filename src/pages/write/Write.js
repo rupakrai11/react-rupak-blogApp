d686164630670fced19ae5eca584058002d88a44
Rupak Rai <rairupak81@gmil.com>
2021-10-23 01:27:32 +0545

    write page is created

--- src/pages/write/Write.js
+++ src/pages/write/Write.js
@@ -1,11 +1,32 @@
-import "./write.css';
+import "./write.css";
+import NewImg from "../../images/nature.jpg";
 
-function Write() {
-    return (
-        <div>
-            
+export default function Write() {
+  return (
+    <div className="wite">
+      <img src={NewImg} alt="New Image" className="writeImg" />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fas fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }}></input>
+          <input
+            type="text"
+            placeholder="title"
+            className="writeInput"
+            autoFocus={true}
+          ></input>
         </div>
-    )
+        <div className="writeFormGroup">
+          <textarea
+            type="text"
+            placeholder="Write Your Blog"
+            className="writeInput writeText"
+          ></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
+    </div>
+  );
 }
-
-export default Write
