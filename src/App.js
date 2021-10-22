a7a16d8f4ab00208f00f379e5f089be20d083a54
Rupak Rai <rairupak81@gmil.com>
2021-10-22 20:53:48 +0545

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TopBar from "./topbar/TopBar";
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/topbar/TopBar.js
@@ -0,0 +1,28 @@
+import "./topbar.css";
+import Avatar from "../images/DSC_0600.JPG";
+export default function TopBar() {
+  return (
+    <>
+      <div className="topbar">
+        <div className="topLeft">
+          <i className="topIcon fab fa-facebook"></i>
+          <i className="topIcon fab fa-twitter"></i>
+          <i className="topIcon fab fa-pinterest"></i>
+          <i className="topIcon fab fa-instagram"></i>
+        </div>
+        <div className="topCenter">
+          <ul className="topList">
+            <li className="topListItem">HOME</li>
+            <li className="topListItem">CONTACT</li>
+            <li className="topListItem">WRITE</li>
+            <li className="topListItem">LOGOUT</li>
+          </ul>
+        </div>
+        <div className="topRight">
+          <img src={Avatar} alt="Avatar" className="topImage" />
+          <i class="searchIcon fas fa-search"></i>
+        </div>
+      </div>
+    </>
+  );
+}
